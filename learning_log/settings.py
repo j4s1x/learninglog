4c2ffac219d449685f685708641873e9836b155f
j4s1x <chessboy14@gmail.com>
2017-06-05 20:52:17 -0600

    set DEBUG=False for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,6 +137,11 @@ if os.getcwd() == '/app':
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+    #allow only herokku to host the project
+    ALLOWED_HOSTS = ['jason-learning.herokuapp.com']
+
+    DEBUG = False
+
     #Allow all host headers
     ALLOWED_HOSTS = ['*']
 
