eaa19aefd7f8c79111bd62a831a02bcbd6426883
j4s1x <chessboy14@gmail.com>
2017-06-05 18:53:32 -0600

    fix equals

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,7 +135,7 @@ if os.getcwd() == '/app':
         'default': dj_database_url.config(default='postgres://localhost')
     }
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #Allow all host headers
     ALLOWED_HOSTS = ['*']
